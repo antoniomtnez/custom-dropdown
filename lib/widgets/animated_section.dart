fb91173445a0dc182fe6f16b73e9e5831c3ee214
José Antonio Martínez <38137118+antoniomtnez@users.noreply.github.com>
2025-10-01 22:59:39 -0400

    [FIX] Remove animations

--- lib/widgets/animated_section.dart
+++ lib/widgets/animated_section.dart
@@ -17,108 +17,31 @@ class _AnimatedSection extends StatefulWidget {
   State<_AnimatedSection> createState() => _AnimatedSectionState();
 }
 
-class _AnimatedSectionState extends State<_AnimatedSection>
-    with SingleTickerProviderStateMixin {
-  AnimationController? _animController;
-  Animation<double>? _animation;
-  bool _isDisposed = false;
-
-  AnimationController get animController => _animController!;
-  Animation<double> get animation => _animation!;
-
+class _AnimatedSectionState extends State<_AnimatedSection> {
   @override
   void initState() {
     super.initState();
-    _isDisposed = false;
-    prepareAnimations();
-    _scheduleInitialExpand();
-  }
-
-  void prepareAnimations() {
-    if (_isDisposed) return;
-
-    _animController = AnimationController(
-      vsync: this,
-      duration: const Duration(milliseconds: 300),
-    )..addStatusListener(_handleAnimationStatus);
-
-    _animation = CurvedAnimation(
-      parent: _animController!,
-      curve: Curves.linearToEaseOut,
-    );
-  }
-
-  void _handleAnimationStatus(AnimationStatus status) {
-    if (_isDisposed || !mounted) return;
-
-    if (status == AnimationStatus.dismissed) {
-      WidgetsBinding.instance.addPostFrameCallback((_) {
-        if (!_isDisposed && mounted && context.findRenderObject() != null) {
-          widget.animationDismissed();
-        }
-      });
-    }
-  }
-
-  void _scheduleInitialExpand() {
-    if (_isDisposed || !mounted) return;
-    
-    WidgetsBinding.instance.addPostFrameCallback((_) {
-      if (!_isDisposed && mounted) {
-        _handleExpand();
-      }
-    });
-  }
-
-  void _handleExpand() {
-    if (_isDisposed || !mounted || _animController == null) return;
-
-    try {
-      final controller = _animController!;
-      if (!controller.isAnimating) {
-        if (widget.expand && controller.status != AnimationStatus.completed) {
-          controller.forward();
-        } else if (!widget.expand && controller.status != AnimationStatus.dismissed) {
-          controller.reverse();
-        }
-      }
-    } catch (e) {
-      // Ignorar errores de animación durante la navegación
-    }
   }
 
   @override
   void didUpdateWidget(_AnimatedSection oldWidget) {
     super.didUpdateWidget(oldWidget);
-    _handleExpand();
+    if (!widget.expand) {
+      widget.animationDismissed();
+    }
   }
 
   @override
   void dispose() {
-    _isDisposed = true;
-    if (_animController != null) {
-      if (_animController!.isAnimating) {
-        _animController!.stop();
-      }
-      _animController!.dispose();
-      _animController = null;
-    }
     super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-    if (_isDisposed || _animation == null) {
+    if (!widget.expand) {
       return const SizedBox.shrink();
     }
 
-    return FadeTransition(
-      opacity: _animation!,
-      child: SizeTransition(
-        axisAlignment: widget.axisAlignment,
-        sizeFactor: _animation!,
-        child: widget.child,
-      ),
-    );
+    return widget.child;
   }
 }
